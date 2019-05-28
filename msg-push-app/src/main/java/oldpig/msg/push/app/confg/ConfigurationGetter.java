70b871e2ffcf7a634d0aa3628c75b21d34fe7b14
zhuwenda <ex-zhuwd@cmfchina.com>
2019-05-28 09:38:58 +0800

    注释

--- msg-push-app/src/main/java/oldpig/msg/push/app/confg/ConfigurationGetter.java
+++ msg-push-app/src/main/java/oldpig/msg/push/app/confg/ConfigurationGetter.java
@@ -5,10 +5,13 @@ import java.util.Set;
 
 public interface ConfigurationGetter {
 
+    /** 获取系统级的通道配置 */
     List<ChannelConfig> getGlobalChannelConfigs();
 
+    /** 获取系统级的事件配置 */
     List<EventConfig> getGlobalEventConfigs();
 
+    /** 通过用户标识、通道ID获取该用户在指定通道上的所需的标识 */
     String getUserIdInChannel(String userId,String channelId);
 
     /** 获取某个用户在某个事件上配置的通道 */
